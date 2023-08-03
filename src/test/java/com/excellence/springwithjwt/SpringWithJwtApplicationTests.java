2adc3a3b87568164a4f625d3364646d02a395bc6
Vijay Singh <aashu.niit4@gmail.com>
2023-08-03 18:03:43 +0530

    first commit

---
+++ src/test/java/com/excellence/springwithjwt/SpringWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
